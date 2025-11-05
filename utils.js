09c4d992c73f1fcc872313b678ee5fc9fbc71bfb
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-06 01:55:58 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T22:54:18.520Z
-Commit: Add export functionality for reports
+Timestamp: 2025-11-05T22:55:56.580Z
+Commit: Patch API endpoint error handling
