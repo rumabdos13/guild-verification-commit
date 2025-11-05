89174c87092af0b751bf3798f274e542a38eddb1
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-05 22:41:43 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:41:38.534Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-05T19:41:41.717Z
+Commit: Fix broken links in documentation
