64590a3211974454420f76d32e77a78da6d1e413
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-05 22:41:51 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:41:40.263Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-05T19:41:49.992Z
+Commit: Fix broken links in documentation
