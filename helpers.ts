11bfefb740fc0205a4a3e952e6fff0decd37b342
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-06 02:04:20 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:02:26.580Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-05T23:04:19.451Z
+Commit: Patch security vulnerability in authentication
