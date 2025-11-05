dd38762253e2d40b0478a3459bc4ae844c6393e6
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-06 02:05:42 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:05:13.358Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-05T23:05:40.764Z
+Commit: Resolve timezone issues in date handling
