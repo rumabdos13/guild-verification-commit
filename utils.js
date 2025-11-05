ed46110f876acb7f26ad32b37fc4dc444070ebf7
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-05 22:45:30 +0300

    Correct typo in configuration file

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:45:13.387Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-05T19:45:28.705Z
+Commit: Correct typo in configuration file
