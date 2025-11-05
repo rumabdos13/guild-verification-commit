8a88eed76dc6e2f47e474c96b795d181e23673ee
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-05 22:45:15 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:44:55.719Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-05T19:45:13.387Z
+Commit: Fix broken links in documentation
