a118bbdab5794ba425a27aebed4f614b04167858
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-05 22:44:58 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:44:42.096Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-05T19:44:57.060Z
+Commit: Fix CSS styling bug on mobile devices
