5f98d96ab16bcffd203eed3159ab158e37690c92
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-06 01:59:57 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T22:59:47.015Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-05T22:59:55.748Z
+Commit: Resolve null pointer exception in user service
