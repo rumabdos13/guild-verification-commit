670d844cef40566e90681e601549d655ae512e13
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-06 02:04:58 +0300

    Add multi-language support

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:04:52.910Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-05T23:04:56.597Z
+Commit: Add multi-language support
