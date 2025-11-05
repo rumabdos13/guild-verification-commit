c3e97cfa3bac10022419f8af148572693dbda77c
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-06 02:02:31 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:02:02.354Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-05T23:02:29.666Z
+Commit: Resolve timezone issues in date handling
