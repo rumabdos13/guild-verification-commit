488aad4ef7fe0f58d7667d1169a1b35e511b0b11
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-06 02:01:48 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T22:59:15.960Z
-Commit: Fix race condition in async operations
+Timestamp: 2025-11-05T23:01:46.793Z
+Commit: Patch security vulnerability in authentication
