be1d09f593ce0448f38988843283dfb11d42ec89
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-06 01:59:14 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T22:56:03.204Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-05T22:59:12.803Z
+Commit: Fix CSS styling bug on mobile devices
