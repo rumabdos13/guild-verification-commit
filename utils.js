6c32840b2936c64210e21fecd8670e1898492691
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-06 02:05:14 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T23:04:46.926Z
-Commit: Fix race condition in async operations
+Timestamp: 2025-11-05T23:05:13.358Z
+Commit: Fix broken links in documentation
