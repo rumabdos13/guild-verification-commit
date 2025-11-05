1020d4b76a0db6f7a3335238599e6406e18e3bbc
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-06 01:54:34 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:45:39.568Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-05T22:54:32.526Z
+Commit: Fix CSS styling bug on mobile devices
