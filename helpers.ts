f1bc880d4cc02c7739d6722e5f6ca1ee35c0a87a
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-06 01:59:17 +0300

    Fix race condition in async operations

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T22:59:11.109Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-05T22:59:15.960Z
+Commit: Fix race condition in async operations
