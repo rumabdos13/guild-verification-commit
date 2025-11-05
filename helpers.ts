5e5494faf018bbcd6eab2dd5f8d7f712850fabbe
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-05 22:44:26 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T19:42:03.136Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-05T19:44:24.784Z
+Commit: Resolve timezone issues in date handling
