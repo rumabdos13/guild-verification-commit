051a20fb210892edf868bf04546a10ee5e76673c
messo <113264242+rumabdos13@users.noreply.github.com>
2025-11-06 01:59:38 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-05T22:59:12.803Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-05T22:59:37.204Z
+Commit: Fix memory leak in event handlers
